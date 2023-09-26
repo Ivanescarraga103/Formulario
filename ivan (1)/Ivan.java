ea87142f2cc09e697235d27f51fd84af1e932a25
Ivanescarraga103 <145467036+Ivanescarraga103@users.noreply.github.com>
2023-09-26 08:16:32 -0600

    Add files via upload

---
+++ ivan (1)/Ivan.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package am.ivan;
+import javax.swing.*;
+/**
+ *
+ * @author fcp
+ */
+public class Ivan extends JFrame {
+public Ivan () {
+     setLayout(null);
+    }
+public static void main(String[]args){
+    Ivan Ivan1=new Ivan();
+    Ivan1.setBounds(10,20,400,300);
+    Ivan1.setVisible(true);
+    Ivan1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+}
+        
+}
